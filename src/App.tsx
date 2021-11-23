76cffb327aad2fcbc5c6be986101aaefce8f0222
luizsmatos <gusstavopdm@gmail.com>
2021-11-23 07:42:30 -0300

    style: Estilização Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import { AppProvider, useAppContext } from './hooks/useAppContext';
+import { AppProvider } from './hooks/useAppContext';
 import Routes from './routes/Routes';
-import GlobalStyles from './styles/global';
+import './styles/global.scss';
 
 const App = () => {
-  const { dataMetahumans } = useAppContext();
   return (
     <BrowserRouter>
       <AppProvider>
         <Routes />
-        <GlobalStyles />
       </AppProvider>
     </BrowserRouter>
   );
