23ecf8e94f78aa5a5cbd3d5772261b42f1fd56d3
luizsmatos <gusstavopdm@gmail.com>
2021-11-24 11:23:48 -0300

    feature: Page Not Found

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,14 @@ import { Routes as Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import DetailsMetaHuman from '../pages/DetailsMetaHuman';
+import NotFound from '../components/NotFound';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" element={<Home />} />
       <Route path="/metahumans/:id" element={<DetailsMetaHuman />} />
+      <Route path="*" element={<NotFound />} />
     </Switch>
   );
 };
